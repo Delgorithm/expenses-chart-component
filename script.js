25afdc339020ed573d978489a26a7e8c8314940e
Delgorithm <delgorithm@gmail.com>
2023-04-29 20:41:08 +0200

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ fetch('data.json')
     .then(function (obj) {
         console.log(obj);
     })
+    .then(amountDisplay => {
+        const amountExpenses = amountDisplay.amount[0];
+    })
     .catch(function (error) {
         console.error('Something went wrong');
         console.log(`Error ${error}`);
-    })
+    });
+
+// Displaying "amount" in the form of a chart
+
