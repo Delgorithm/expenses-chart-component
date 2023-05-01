ee755fdd03b9739560c39fe1a8bace261e06a843
Delgorithm <delgorithm@gmail.com>
2023-05-01 15:56:57 +0200

    script.js

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ fetch('data.json')
     .catch(function (error) {
         console.error(`Something went wrong : ${error}`);
     });
+
+// Chart
+
+const clrCyan = getComputedStyle(document.documentElement).getPropertyValue('--clr-cyan');
+console.log(clrCyan);
+
+
+const dayOfTheWeek = document.getElementById('test');
+
+dayOfTheWeek.addEventListener('click', () => {
+    dayOfTheWeek.style.backgroundColor = clrCyan;
+});
+
+
